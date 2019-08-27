8d7f97c3221dd1ebe4868569ddaa63e94b7be0b7
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-27 19:33:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,19 +7,17 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-
+    @@song_count += 1
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
-
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
-    @@song_count += 1
   end
 
   def self.song_count
