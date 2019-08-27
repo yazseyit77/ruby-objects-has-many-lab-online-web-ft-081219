cfd03209ab5f6badd06002fc9e4f7261ec05c037
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-27 13:58:09 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,9 +17,9 @@ class Author
     @posts
   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    add_song(song)
+  def add_post_by_name(name)
+    post = Post.new(name)
+    add_post(post)
   end
 
   def self.song_count
