0bebd4c5d75d18feede9c0103bf5949ac484c14c
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-27 19:36:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,16 +1,17 @@
 class Author
   attr_accessor :name
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
-
   end
 
   def add_post(post)
     @posts << post
     post.author = self
-
+    @@post_count += 1
   end
 
   def posts
@@ -20,10 +21,11 @@ class Author
   def add_post_by_title(title)
     post = Post.new(title)
     add_post(post)
+    @@post_count += 1
   end
 
   def self.post_count
-
+    @@post_count
   end
 
   def posts
