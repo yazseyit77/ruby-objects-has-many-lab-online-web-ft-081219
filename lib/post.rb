3a761313cdbbf0646cd0a7cf586d7ddd7c9b8948
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-27 13:59:02 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :artist, :name, :genre
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @all
+  end
+
+  def artist_name
+    if self.artist.name == artist.name
+      return artist.name
+    else
+      nil
+    end
+  end
+
+end
